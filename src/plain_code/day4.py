ad69de2c2923306e670e213f9e48f18e57cdc3f6
Kevin Chen <1354016594@qq.com>
2021-02-01 11:17:33 +0800

    Update day4.py

---
+++ src/plain_code/day4.py
@@ -0,0 +1,27 @@
+a = 100
+b = 100
+
+c = a + b
+c = a - b
+c = a * b
+c = a / b
+
+c = a ** b
+c = a // b
+
+import math
+
+c = math.log(8, 2)
+c = math.cos(math.pi)
+
+a = True
+b = False
+c = a and b
+c = a or b
+c = a ^ b
+
+a > b
+a < b
+a >= b
+a == b
+a != b
